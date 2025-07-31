16408c943d8a2ad21ddd15fbc84641e18b6827b5
johnaboyeji001 <johnlwtb@gmail.com>
2025-07-31 17:15:03 +0100

    Add files via upload

---
+++ python_assignment_week1_Aboyeji_John.py
@@ -0,0 +1,23 @@
+first_number = input(
+    "enter a number: "
+    )
+second_number = input(
+    "enter another number: "
+)
+arithmetc_sign = input(
+    "enter an arithmetic sign (+,-,*,/,%): "
+)
+
+
+if arithmetc_sign == "+":
+    print(first_number ,arithmetc_sign ,second_number," = ",int(first_number) + int(second_number))
+elif arithmetc_sign == "-":
+    print(first_number ,arithmetc_sign ,second_number," = ",int(first_number) - int(second_number))
+elif arithmetc_sign == "*":
+    print(first_number ,arithmetc_sign ,second_number," = ",int(first_number) * int(second_number))
+elif arithmetc_sign == "/":
+    print(first_number ,arithmetc_sign ,second_number," = ",int(first_number) / int(second_number))
+elif arithmetc_sign == "%":
+    print(first_number ,arithmetc_sign ,second_number," = ",int(first_number) % int(second_number))
+else:
+    print("invalid arithmetic sign")
